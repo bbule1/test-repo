32ccbccf2ce65f4a08cdeb093f7e4c8e93c1970e
Brandon Bule <brandonbule92@aim.com>
2024-11-30 18:11:20 +0000

    test repo

---
+++ day1_2023/day_file.py
@@ -0,0 +1,24 @@
+if __name__ == "__main__":
+    import re
+
+    input_file = 'input_file.txt'
+
+    with open(input_file) as f:
+        list_of_digit = []
+
+        for word in f.read().splitlines():
+            output = re.findall(r'\d', word)
+            final_digit = output[0] + output[-1]
+            final_int = int(final_digit)
+            list_of_digit.append(final_int)
+
+
+        print(list_of_digit[::-1])
+
+
+
+
+
+
+
+
